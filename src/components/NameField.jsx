3ecb197687a58b6e25fdc3881d96e034ef956733
gperera89 <80236223+gperera89@users.noreply.github.com>
2024-02-06 10:53:17 +0800

    Refactor code to improve performance and readability

--- src/components/NameField.jsx
+++ src/components/NameField.jsx
@@ -9,12 +9,20 @@ function NameField() {
 	};
 
 	return (
-		<div className='rounded-md px-3 pb-1.5 pt-2.5 shadow-sm ring-1 ring-inset ring-slate-500 focus-within:ring-2 focus-within:ring-[#0047AB] dark:focus-within:ring-[#4682B4] w-1/4'>
+		<div className='relative rounded-md px-3 pb-1.5 pt-2.5 shadow-sm ring-1 ring-inset ring-slate-500 focus-within:ring-2 focus-within:ring-[#0047AB] dark:focus-within:ring-[#4682B4] w-1/4'>
+			{name && (
+				<label
+					htmlFor='name'
+					className={`absolute -top-2 left-2 inline-block bg-white dark:bg-slate-900 px-1 text-xs font-medium text-gray-500`}>
+					Name / Candidate Number
+				</label>
+			)}
 			<input
+				type='text'
 				value={name}
 				id='name'
-				className='block w-full border-0 p-0 text-slate-900 dark:text-white placeholder:text-slate-500 dark:bg-slate-900 focus:ring-0 sm:text-sm sm:leading-6 text-center'
 				placeholder='Name / Candidate Number'
+				className='block w-full border-0 p-0 text-slate-900 dark:text-white placeholder:text-slate-500 dark:bg-slate-900 focus:ring-0 sm:text-sm sm:leading-6 text-center'
 				onChange={handleNameChange}
 			/>
 		</div>
